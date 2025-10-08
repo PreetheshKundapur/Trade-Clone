205fe38a0ea90d4b0b6784f01e69e02a3d49c1d1
PreetheshKundapur <preetheshpk51@gmail.com>
2025-10-08 20:34:54 +0530

    deployed link

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,7 @@ const MONGO_URI = process.env.MONGO_URL;
 
 // -------------------- MIDDLEWARE --------------------
 app.use(cors({
-  origin: ["http://trade-clone-kfyi-6gxqi6iem-preethesh-pks-projects.vercel.app", "http://trade-clone-z5ue.vercel.app"], // frontend + dashboard
+  origin: ["https://trade-clone-easy.vercel.app", "http://trade-clone-z5ue.vercel.app"], // frontend + dashboard
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true, // allow cookies
 }));
